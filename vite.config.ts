e382cc458a0f9e586ec55b29f2ce9588cdb9429f
ali najib hegazy <alinajibhegazy4@gmail.com>
2024-08-12 16:01:21 +0300

    add path aliases by typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
   plugins: [
     react(),
     svgr(),
